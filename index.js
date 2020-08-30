5a55ec628b4e1851e73d864277f49782eab3e5e0
Patricia Arnedo <paaarnedo@gmail.com>
2020-08-30 21:58:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase())
     return "I can't hear you!"
+    
+  if (string === string.toUpperCase())
+    return "YES INDEED!"
+    
+  if (string === "I Love you, Grandma.")
+    return "I love you, too."
   
-
 }
