3265b8b13a3b5a0916e79ce4e5289cc9b64fc3ee
Patricia Arnedo <paaarnedo@gmail.com>
2020-08-30 21:55:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ function logWhisper(string) {
   console.log(whisper(string))
 }
 
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase())
+    return "I can't hear you!"
+  
+
+}
